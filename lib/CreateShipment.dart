5b91ac36c754d6a62863f88cf0d0168075f5802e
GOPINATHSWAMINATHAN <gopinathswaminadane@gmail.com>
2019-12-26 17:37:00 +0530

    Created Shipment

---
+++ lib/CreateShipment.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateShipment extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+
+        ),
+      ),
+    );
+  }
+}
